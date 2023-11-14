d2093516a8fcb9853aad2d1d6d22be6d57fded17
Kyllingene <fuzzymuffin343@gmail.com>
2023-11-14 11:55:26 -0800

    Made timer I/O parallel

--- src/timer.rs
+++ src/timer.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 use chrono::{DateTime, Duration, Local};
 use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyEventKind};
 
@@ -5,69 +7,106 @@ use crate::common::sleep;
 use crate::format::{dur, time};
 use crate::print::Printer;
 
+struct TimerState {
+    time: Duration,
+    duration: Duration,
+    increment: Duration,
+
+    cancel: bool,
+    paused: bool,
+
+    printer: Printer,
+}
+
+fn read_keys(state: Arc<Mutex<TimerState>>) {
+    loop {
+        let key = read().unwrap();
+        let mut state = state.lock().unwrap();
+        match key {
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('p') | KeyCode::Char(' '),
+                kind: KeyEventKind::Press,
+                ..
+            }) => {
+                if !state.paused {
+                    let left = &(state.time - state.duration);
+                    state.printer.erase(format!(" {} PAUSED", dur::time(left)));
+                }
+
+                state.paused = !state.paused;
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('q') | KeyCode::Esc,
+                kind: KeyEventKind::Press,
+                ..
+            }) => {
+                let left = &(state.time - state.duration);
+                let dur = dur::time(&state.duration);
+                state.printer.print(format!(
+                    "\x07Timer for {} cancelled ({} left)",
+                    dur,
+                    dur::time(left),
+                ));
+                state.cancel = true;
+                return;
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Right | KeyCode::Char('a'),
+                kind: KeyEventKind::Press,
+                ..
+            }) => {
+                state.time = state.time - (state.increment * 5);
+                let left = &(state.time - state.duration);
+                state.printer.erase(format!(" {}", dur::time(left)));
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Left | KeyCode::Char('d'),
+                kind: KeyEventKind::Press,
+                ..
+            }) => {
+                state.time = state.time + (state.increment * 5);
+                let left = &(state.time - state.duration);
+                state.printer.erase(format!(" {}", dur::time(left)));
+            }
+            _ => (),
+        }
+    }
+}
+
 pub fn timer(duration: Duration) {
     print!("Timer started at ");
     crate::clock::time();
 
-    let mut printer = Printer::new();
+    let state = Arc::new(Mutex::new(TimerState {
+        time: Duration::zero(),
+        duration,
+        increment: Duration::seconds(1),
 
-    let mut time = Duration::zero();
+        paused: false,
+        cancel: false,
 
-    let mut paused = false;
+        printer: Printer::new(),
+    }));
 
-    let second = Duration::seconds(1);
+    let read_state = state.clone();
+    std::thread::spawn(move || read_keys(read_state));
+
     loop {
-        if poll(std::time::Duration::ZERO).unwrap() {
-            match read().unwrap() {
-                Event::Key(KeyEvent {
-                    code: KeyCode::Char('p') | KeyCode::Char(' '),
-                    kind: KeyEventKind::Press,
-                    ..
-                }) => {
-                    if !paused {
-                        printer.erase(format!(" {} PAUSED", dur::time(&(time - duration))));
-                    }
-
-                    paused = !paused;
-                }
-                Event::Key(KeyEvent {
-                    code: KeyCode::Char('q') | KeyCode::Esc,
-                    kind: KeyEventKind::Press,
-                    ..
-                }) => {
-                    printer.print(format!(
-                        "\x07Timer for {} cancelled ({} left)",
-                        dur::time(&duration),
-                        dur::time(&(time - duration))
-                    ));
-                    break;
-                }
-                Event::Key(KeyEvent {
-                    code: KeyCode::Right | KeyCode::Char('a'),
-                    kind: KeyEventKind::Press,
-                    ..
-                }) => {
-                    time = time - (second * 10);
-                }
-                Event::Key(KeyEvent {
-                    code: KeyCode::Left | KeyCode::Char('d'),
-                    kind: KeyEventKind::Press,
-                    ..
-                }) => {
-                    time = time + (second * 10);
-                }
-                _ => (),
+        sleep(1.0);
+        let mut state = state.lock().unwrap();
+        if !state.paused {
+            if state.cancel {
+                break;
             }
-        }
 
-        if !paused {
-            sleep(1.0);
+            state.time = state.time + state.increment;
+            let left = &(state.time - state.duration);
+            state.printer.erase(format!(" {}", dur::time(left)));
 
-            time = time + second;
-            printer.erase(format!(" {}", dur::time(&(time - duration))));
-
-            if time >= duration {
-                printer.print(format!("\x07Timer for {} complete", dur::time(&duration)));
+            if state.time >= state.duration {
+                let dur = dur::time(&state.duration);
+                state.printer.print(format!("\x07Timer for {} complete", dur));
+                state.cancel = true;
                 break;
             }
         }
